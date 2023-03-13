a6feca83ad430d0938a0ddd04df9ee3830647cd9
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-03-13 12:01:25 +0100

    print for debug

--- workflow/scripts/scan_mutations.py
+++ workflow/scripts/scan_mutations.py
@@ -35,8 +35,8 @@ def main(fname_vcf, fname_csv, fname_yaml):
 
     elif  fname_vcf.split(".")[-1]=="tsv":
         df_vcf = pd.read_csv(fname_vcf, sep='\t')
-        print(df_vcf)
-        df_mut = df_vcf[df_vcf['Pos'].isin(positions_of_interest)]
+        if df_vcf.shape[0]>0:
+            df_mut = df_vcf[df_vcf['Pos'].isin(positions_of_interest)]
 
     df_mut.to_csv(fname_csv)
 
