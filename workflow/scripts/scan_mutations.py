732f32e6ef06c7228181c4bf190a208db0a34594
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2023-03-13 10:25:11 +0100

    correct and add scan_mutations

---
+++ workflow/scripts/scan_mutations.py
@@ -0,0 +1,43 @@
+"""
+Input:
+    -- snvs.vcf outputted by VILOCA
+    -- yaml with mutation definitions
+
+Output:
+    -- per sample csv file with the mutation calls and their respective frequency
+"""
+import yaml
+import pandas as pd
+from fuc import pyvcf
+
+def parse_yaml(fname_yaml):
+
+    yaml_file = open(fname_yaml)
+    parsed_yaml_file = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    dict_mut =parsed_yaml_file.get('mut')
+
+    return dict_mut
+
+
+
+
+
+def main(fname_vcf, fname_csv, fname_yaml):
+
+    # read mutations
+    dict_mut = parse_yaml(fname_yaml)
+    positions_of_interest = [int(x) for x in list(dict_mut.keys())]
+
+    # vcf into dataframe
+    df_vcf = pyvcf.VcfFrame.from_file(f_snv_vcf).df
+
+    df_mut = df_vcf[df_vcf['POS'].isin(positions_of_interest)]
+
+    df_mut.to_csv(fname_csv)
+
+if __name__ == "__main__":
+    main(
+        snakemake.input.fname_snvs_vcf,
+        snakemake.output.fname_mutations,
+        snakemake.params.fname_mutation_definitions,
+    )
